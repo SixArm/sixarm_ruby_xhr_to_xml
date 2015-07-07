3ed2de6acabbacb785f9d4477accc014e1c0c8fe
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:59 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_xhr_to_xml_test.rb
+++ test/sixarm_ruby_xhr_to_xml_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_xhr_to_xml'
+require "sixarm_ruby_xhr_to_xml"
 
 class Testing < Test::Unit::TestCase
 
